711407e969bbe99261292b28ecfb4fbd358727d2
Tetiana Savinykh <117289657+Galebonchik@users.noreply.github.com>
2023-03-31 22:23:47 +0200

    solving task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
+const paletteGalery = document.querySelector(`.gallery`);
+const cardsOfGallery = createGallery(galleryItems);
+
+paletteGalery.insertAdjacentHTML(`beforeend`, cardsOfGallery);
+
+function createGallery(galleryItems) {
+    return galleryItems.map(
+    ({ preview, original, description }) => `
+<a class="gallery__item" href="${original}">
+  <img data-caption="${description}" class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+`
+  )
+  .join('');
+};
+ 
+let lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250, });
+
+
 console.log(galleryItems);
+
